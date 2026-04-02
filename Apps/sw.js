4331147275912c45d613be1aefd83b03494b725d
Matthew <itsmat-github@gmx.com>
2026-04-02 22:05:12 +0100

    Update sw.js

--- Apps/sw.js
+++ Apps/sw.js
@@ -1,17 +1,5 @@
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('app-drawer').then(cache => {
-      return cache.addAll([
-        './',
-      ]);
-    })
-  );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
-  );
-});
+self.addEventListener('fetch', e => {});
